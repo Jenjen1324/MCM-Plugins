49bbed8477aec95684f0ab77ad37539172cbbc08
Jenjen1324 <northcodedotno@gmail.com>
2013-02-16 20:48:03 +0100

    Started LoginInfo

---
+++ MCM-Plugins/Backup_LoginInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MCManager.Backups;
+
+namespace MCM_Plugins
+{
+    class Backup_LoginInfo : IBackup
+    {
+        private string username;
+        private string displayname;
+        private string password;
+
+        public void Extract()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetBackupType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFilePath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBackupFormat GetFormat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MCM-Plugins/Format_LoginInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MCManager.Backups;
+
+namespace MCM_Plugins
+{
+    class Format_LoginInfo : IBackupFormat
+    {
+        public IBackup CreateBackup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFormatName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBackup Load(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string file, IBackup backup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte getSignature()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
