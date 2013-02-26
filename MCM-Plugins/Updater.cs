78ae2c5e9a8af224cc0ea08deefbc35ee2a8a3a9
Jenjen1324 <northcodedotno@gmail.com>
2013-02-26 22:05:35 +0100

    Updated ver.txt

--- MCM-Plugins/Updater.cs
+++ MCM-Plugins/Updater.cs
@@ -12,7 +12,7 @@ namespace MCM_Plugins
     public class Updater : IUpdater
     {
         string path;
-        const string LocalVersion = "0.3";
+        const string LocalVersion = "0.5";
         const string configNode = "MCM Plugins";
 
         public bool CheckForUpdates()
